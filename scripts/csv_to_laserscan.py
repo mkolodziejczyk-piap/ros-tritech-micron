536dec13ec15a20c36749ea76d82b7bd4dc75218
Anass Al <mr@anassinator.com>
2015-04-19 00:19:48 -0400

    Fix hacky code

--- scripts/csv_to_laserscan.py
+++ scripts/csv_to_laserscan.py
@@ -8,6 +8,7 @@ import sys
 import rospy
 import numpy as np
 from datetime import datetime
+from collections import namedtuple
 from sensor_msgs.msg import LaserScan
 
 __author__ = "Anass Al-Wohoush"
@@ -23,34 +24,44 @@ class Slice(object):
 
     """Scan slice."""
 
-    def __init__(self, *args):
+    def __init__(self, row, min_distance=1, min_intensity=50):
         """Constructs Slice object.
 
         Args:
-            *args: Columns from CSV log.
+            row: Current row as column array from CSV log.
+            min_distance: Minimum distance in meters.
+            min_intensity: Minimum intensity.
         """
-        self.sof = str(args[0])
-        self.timestamp = datetime.strptime(args[1], "%H:%M:%S.%f")
-        self.node = int(args[2])
-        self.status = int(args[3])
-        self.hdctrl = int(args[4])
-        self.range = float(args[5]) / 10
-        self.gain = int(args[6])
-        self.slope = int(args[7])
-        self.ad_low = int(args[8])
-        self.ad_span = int(args[9])
-        self.left_limit = int(args[10])
-        self.right_limit = int(args[11])
-        self.steps = int(args[12])
-        self.heading = int(args[13])
-        self.num_bins = int(args[14])
-        self.data = map(int, args[15:])
-        argmax = np.argmax(self.data[60:]) + 60
-        self.max = argmax * self.range / self.num_bins
-        self.max_intensity = self.data[argmax]
+        # Extract the fields in order.
+        self.sof = str(row[0])
+        self.timestamp = datetime.strptime(row[1], "%H:%M:%S.%f")
+        self.node = int(row[2])
+        self.status = int(row[3])
+        self.hdctrl = int(row[4])
+        self.range = float(row[5]) / 10
+        self.gain = int(row[6])
+        self.slope = int(row[7])
+        self.ad_low = int(row[8])
+        self.ad_span = int(row[9])
+        self.left_limit = int(row[10])
+        self.right_limit = int(row[11])
+        self.steps = int(row[12])
+        self.heading = int(row[13])
+        self.num_bins = int(row[14])
+        self.data = map(int, row[15:])
 
         # Direction is encoded as the third bit in the HDCtrl bytes.
-        self.scanright = self.hdctrl & 0b100 > 0
+        self.clockwise = self.hdctrl & 0b100 > 0
+
+        # Determine range of maximum intensity.
+        min_range = int(min_distance / self.range * self.num_bins)
+        data = [
+            intensity if intensity > min_intensity and index > min_range else 0
+            for index, intensity in enumerate(self.data)
+        ]
+        argmax = np.argmax(data)
+        self.max = argmax * self.range / self.num_bins
+        self.max_intensity = self.data[argmax]
 
     def __str__(self):
         """Returns string representation of Slice."""
@@ -68,7 +79,7 @@ class Scan(object):
         self.num_bins = None
         self.left_limit = None
         self.right_limit = None
-        self.previous_direction = None
+        self.clockwise = None
         self.slices = []
 
     def empty(self):
@@ -99,17 +110,14 @@ class Scan(object):
                 or scan_slice.num_bins != self.num_bins
                 or scan_slice.steps != self.steps
                 or scan_slice.left_limit != self.left_limit
-                or scan_slice.right_limit != self.right_limit):
+                or scan_slice.right_limit != self.right_limit
+                or scan_slice.clockwise != self.clockwise):
             self.range = scan_slice.range
             self.num_bins = scan_slice.num_bins
             self.steps = scan_slice.steps
             self.left_limit = scan_slice.left_limit
             self.right_limit = scan_slice.right_limit
-            self.slices = [scan_slice]
-            return
-
-        # Reset if direction is switched.
-        if self.slices[-1].scanright != scan_slice.scanright:
+            self.clockwise = scan_slice.clockwise
             self.slices = [scan_slice]
             return
 
@@ -118,7 +126,16 @@ class Scan(object):
             self.slices.pop(0)
         self.slices.append(scan_slice)
 
-    def to_laser_scan(self, queue=0, frame="odom"):
+    def to_laser_scan(self, frame, queue=0):
+        """Converts current scan to LaserScan message.
+
+        Args:
+            frame: Frame name.
+            queue: Queue size (0 means full scan).
+
+        Returns:
+            LaserScan.
+        """
         # Return nothing if empty.
         if len(self.slices) < queue or self.empty():
             return None
@@ -133,8 +150,6 @@ class Scan(object):
         queued_slices = sorted(
             self.slices, key=lambda x: x.timestamp
         )[-1 * queue:]
-        if not self.slices[0].scanright:
-            queued_slices.reverse()
 
         # Set time and time increments.
         scan.time_increment = (
@@ -144,31 +159,60 @@ class Scan(object):
             queued_slices[-1].timestamp - queued_slices[0].timestamp
         ).total_seconds()
 
-        # Determine angular range.
-        angle_min = 0 # hack
-        angle_max = 6400 # hack hack
-        if angle_max < angle_min:
-            angle_max += 6400
-        scan.angle_min = sonar_angle_to_rad(angle_min)
-        scan.angle_max = sonar_angle_to_rad(angle_max)
+        # Set angular range.
+        scan.angle_min = sonar_angle_to_rad(0)
+        scan.angle_max = sonar_angle_to_rad(6400)
         scan.angle_increment = sonar_angle_to_rad(self.steps)
 
-        # Set ranges and intensities.
+        # Determine ranges and intensities.
+        num_of_headings = 6400 / self.steps
         scan.range_max = self.range
         scan.range_min = 0.1
-        scan.ranges = [
-            scan_slice.max
-            for scan_slice in sorted(queued_slices, key=lambda x: (x.heading % 6400)) # hack hack hack
-        ]
-        scan.intensities = [
-            scan_slice.max_intensity
-            for scan_slice in queued_slices
-        ]
+        scan.ranges = [0 for i in range(num_of_headings)]
+        scan.intensities = [0 for i in range(num_of_headings)]
+        for scan_slice in queued_slices:
+            index = scan_slice.heading / self.steps
+            scan.ranges[index] = scan_slice.max
+            scan.intensities[index] = scan_slice.max_intensity
 
         return scan
 
 
-def main(path, queue, rate):
+def get_parameters():
+    """Gets relevant ROS parameters into a named tuple.
+
+    Relevant properties are
+
+    Returns:
+        Named tuple with the following properties:
+            path: Path to CSV log.
+            queue: Queue for sector scan message.
+            rate: Publishing rate in Hz.
+            frame: Frame name.
+    """
+    options = namedtuple("Parameters", ["path", "queue", "rate", "frame"])
+
+    options.path = rospy.get_param("~csv", None)
+    options.queue = rospy.get_param("~queue", 10)
+    options.rate = rospy.get_param("~rate", 30)
+    options.frame = rospy.get_param("~frame", "odom")
+
+    return options
+
+
+def main(path, queue, rate, frame):
+    """Parses scan logs and publishes LaserScan messages at set frequency.
+
+    This publishes on two topics:
+        /sonar/full: A full 360 degree scan.
+        /sonar/sector: A queued scan with the past few headings.
+
+    Args:
+        path: Path to CSV log.
+        queue: Queue for sector scan message.
+        rate: Publishing rate in Hz.
+        frame: Frame name.
+    """
     # Create publisher.
     full_pub = rospy.Publisher("sonar/full", LaserScan, queue_size=10)
     sector_pub = rospy.Publisher("sonar/sector", LaserScan, queue_size=10)
@@ -182,18 +226,21 @@ def main(path, queue, rate):
         next(info)
 
         for row in info:
+            # Break cleanly if requested.
             if rospy.is_shutdown():
                 break
-            scan_slice = Slice(*row)
+
+            # Parse row.
+            scan_slice = Slice(row, min_distance=1, min_intensity=50)
             scan.add(scan_slice)
 
             # Publish full scan.
-            laser_scan = scan.to_laser_scan(queue=0)
+            laser_scan = scan.to_laser_scan(frame)
             if laser_scan:
                 full_pub.publish(laser_scan)
 
             # Publish sector scan.
-            laser_scan = scan.to_laser_scan(queue=queue)
+            laser_scan = scan.to_laser_scan(frame, queue=queue)
             if laser_scan:
                 sector_pub.publish(laser_scan)
 
@@ -204,27 +251,14 @@ if __name__ == "__main__":
     # Start node.
     rospy.init_node("sonar")
 
-    # Get path to CSV.
-    if rospy.has_param("~csv"):
-        path = rospy.get_param("~csv")
-    else:
-        rospy.logfatal("Please specify path to CSV")
+    # Get parameters.
+    options = get_parameters()
+    if options.path is None:
+        rospy.logfatal("Please specify a file.")
         sys.exit(-1)
 
-    # Get queue size.
-    if rospy.has_param("~queue"):
-        queue = rospy.get_param("~queue")
-    else:
-        queue = 10
-
-    # Get rate.
-    if rospy.has_param("~rate"):
-        rate = rospy.get_param("~rate")
-    else:
-        rate = 30
-
     try:
-        main(path, queue, rate)
+        main(options.path, options.queue, options.rate, options.frame)
     except IOError:
         rospy.logfatal("Could not find file specified")
     except rospy.ROSInterruptException:
