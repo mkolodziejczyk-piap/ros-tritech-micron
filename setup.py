0c65532d6f5c01082d25bb6a0e128020bcadb04e
Anass Al <dev@anassinator.com>
2018-04-04 18:22:42 -0400

    Fix build formatting errors

--- setup.py
+++ setup.py
@@ -7,8 +7,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 __author__ = "Anass Al-Wohoush"
 
 d = generate_distutils_setup(
-    packages=["tritech_micron"], package_dir={
-        "": "src"
-    })
+    packages=["tritech_micron"], package_dir={"": "src"})
 
 setup(**d)
